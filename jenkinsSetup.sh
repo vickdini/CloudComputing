4a85d95c294889f21b47c36e1890fc85be318d5a
Vick Dini <vickdini@gmail.com>
2023-05-02 11:19:42 +0200

    Update jenkinsSetup.sh

--- jenkinsSetup.sh
+++ jenkinsSetup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install Java
-sudo apt update
-sudo apt install openjdk-11-jre
+apt update
+apt install -y openjdk-11-jre
 
 # Install Jenkins
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
@@ -10,8 +10,8 @@ curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins
+apt-get update
+apt-get install jenkins
 
 # Get password for the Jenkins website
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+cat /var/lib/jenkins/secrets/initialAdminPassword
