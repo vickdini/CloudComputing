abab1c3cc68987f56c91a83127c2252c69b41b71
Vick Dini <vickdini@gmail.com>
2023-05-11 15:58:53 +0200

    Update ansibleAWXSetup.sh

--- ansibleAWXSetup.sh
+++ ansibleAWXSetup.sh
@@ -3,6 +3,8 @@
 # Update package lists
 sudo apt update
 
+# Run the VM with the host processor
+
 apt install -y docker.io docker-compose ansible nodejs npm unzip python3-pip git pwgen
 pip3 install awxkit setuptools-scm
 
@@ -10,6 +12,14 @@ wget https://github.com/ansible/awx/archive/refs/tags/22.2.0.zip
 unzip 22.2.0.zip
 cd awx-22.2.0
 
+##Values are written out to tools/docker-compose/_sources/secrets/
+
 git init
 
 make docker-compose-build
+
+make docker-compose
+
+docker exec tools_awx_1 make clean-ui ui-devel
+
+echo "The UI can be reached in your browser at https://localhost:8043/#/home"
