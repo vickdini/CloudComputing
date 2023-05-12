c0d6fc8c916ca652e4c9e8939505db50df82ec43
Vick Dini <vickdini@gmail.com>
2023-05-12 17:49:23 +0200

    Update and rename ansibleAWXSetupMinikube to ansibleAWXSetupMinikube.sh

---
+++ ansibleAWXSetupMinikube.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install Docker
+sudo apt install -y docker.io
+
+# Install Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
+
+minikube config set driver docker
+minikube delete
+minikube start
