21156310901e6c85b8a8b0d31c8f0e6e74cecb37
Vick Dini <vickdini@gmail.com>
2023-05-13 10:21:09 +0200

    Update ansibleAWXSetupMinikube.sh

--- ansibleAWXSetupMinikube.sh
+++ ansibleAWXSetupMinikube.sh
@@ -18,8 +18,8 @@ apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 resources:
   # Find the latest tag here: https://github.com/ansible/awx-operator/releases
-  - https://github.com/ansible/awx-operator/tree/devel/config/default?ref=latest
-  #- github.com/ansible/awx-operator/config/default?ref=<tag>
+  #- https://github.com/ansible/awx-operator/tree/devel/config/default?ref=latest
+  - https://github.com/ansible/awx-operator/config/default?ref=latest
 
 # Set the image tags to match the git version from above
 images:
