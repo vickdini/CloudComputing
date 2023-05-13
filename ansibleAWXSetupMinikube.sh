ad61cbc5dc2fcd59749555b9b8b99acf5c7fcb74
Vick Dini <vickdini@gmail.com>
2023-05-13 10:00:37 +0200

    Update ansibleAWXSetupMinikube.sh

--- ansibleAWXSetupMinikube.sh
+++ ansibleAWXSetupMinikube.sh
@@ -13,22 +13,23 @@ minikube config set driver docker
 minikube delete
 minikube start --force
 
-alias kubectl="minikube kubectl --"
-
 cat <<EOF | sudo tee kustomization.yaml
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 resources:
   # Find the latest tag here: https://github.com/ansible/awx-operator/releases
-  - github.com/ansible/awx-operator/config/default?ref=<tag>
+  - github.com/ansible/awx-operator/tree/devel/config/default?ref=latest
+  #- github.com/ansible/awx-operator/config/default?ref=<tag>
 
 # Set the image tags to match the git version from above
 images:
   - name: quay.io/ansible/awx-operator
-    newTag: <tag>
+    newTag: latest
 
 # Specify a custom namespace in which to install AWX
 namespace: awx
 EOF
+
+alias kubectl="minikube kubectl --"
 
 kubectl apply -k .
