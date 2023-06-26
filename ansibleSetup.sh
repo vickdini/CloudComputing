dd56c33d9b205d4bde4948bef63f87a21e76c1ac
Vick Dini <vickdini@gmail.com>
2023-06-26 11:04:25 +0200

    Update ansibleSetup.sh

--- ansibleSetup.sh
+++ ansibleSetup.sh
@@ -4,10 +4,7 @@
 sudo apt update
 
 # Install Python and other dependencies
-sudo apt install -y python3 python3-pip python3-dev libffi-dev libssl-dev
-
-# Upgrade pip
-sudo pip3 install --upgrade pip
+sudo apt install -y python3 python3-pip
 
 # Install Ansible
 sudo pip3 install ansible
