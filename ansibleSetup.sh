c759c92223299a1b0f9e9e261d95a16e43d68ebf
Vick Dini <vickdini@gmail.com>
2023-06-26 11:11:28 +0200

    Update ansibleSetup.sh

--- ansibleSetup.sh
+++ ansibleSetup.sh
@@ -8,3 +8,8 @@ sudo apt install -y python3 python3-pip
 
 # Install Ansible
 sudo pip3 install ansible
+
+# Create user
+useradd ansible
+echo password | passwd --stdin ansible
+echo "ansible ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/ansible
