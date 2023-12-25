7ffe3d81ee8a6f13ff4c0082895d186f19612749
Vick Dini <vickdini@gmail.com>
2023-12-25 20:48:52 +0100

    Update K8S+Docker_Master_Setup.sh

--- K8S+Docker_Master_Setup.sh
+++ K8S+Docker_Master_Setup.sh
@@ -12,8 +12,8 @@ systemctl start docker
 systemctl enable docker
 
 # Install Kubernetes
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt update
 apt install -y kubelet kubeadm kubectl kubernetes-cni
 apt-mark hold kubelet kubeadm kubectl kubernetes-cni
